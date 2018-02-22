1530369c82fe3fea1d82375473c1ee4497ca51fc
Mggeroff <roman.mager@outlook.com>
2018-02-22 17:46:51 +0300

    All tests pass. Still need some refactoring

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    let regex = /[^\^]\d+/g;
+    let numMatch;
+    let tempArr = [];
+    while (numMatch = regex.exec(equation.replace(/[^0-9-+x]/g, ''))) {
+        tempArr.push(numMatch[0]);
+    }
+
+    let a = tempArr[0];
+    let b = tempArr[2];
+    let c = tempArr[3];
+
+    let Discriminant = (Math.pow(b, 2) - 4 * a * c);
+
+    let x1 = Math.round(((-b + Math.round(Math.sqrt(Discriminant))) / (2 * a)));
+    let x2 = Math.round(((-b - Math.round(Math.sqrt(Discriminant))) / (2 * a)));
+
+    let result = [];
+    result.push(x1);
+    result.push(x2);
+
+    return result.sort(function (a, b) {
+        return a - b;
+    })
 }
